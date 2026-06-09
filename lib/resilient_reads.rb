b31d4528880bb46cb4cbb1dd45847732c05928a6
Jamie Puckett <chronicaust@yahoo.com>
2026-06-08 21:51:49 -0500

    add initial gem structure with configuration, README, and test setup

---
+++ lib/resilient_reads.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "resilient_reads/version"
+
+module ResilientReads
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/resilient_reads/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ResilientReads
+  VERSION = "0.1.0"
+end
